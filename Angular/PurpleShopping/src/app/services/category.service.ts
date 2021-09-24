99bafdf73fd30e0cf255b5122a10d4f35a25e314
Murat Necip Arcan <muratneciparcan@gmail.com>
2021-09-24 15:39:55 +0300

    Angular Changes - Adjusting Networking Layers Adding a Token Interceptor Implementing a custom http caller and helpers. Redesigning service layer Adding Toaster to show success and error message which catch by interceptor. Adding mapped endpoint urls to service layer. Adjusting environment file.

--- Angular/PurpleShopping/src/app/services/category.service.ts
+++ Angular/PurpleShopping/src/app/services/category.service.ts
@@ -1,19 +1,40 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { HttpCaller } from '../common/http-caller';
 import { Category } from '../models/Category';
+import { SettingsService } from './settings.service';
+import { Response } from '@angular/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor(private http:HttpClient) { }
-  path= 'https://localhost:44389/api/';
+  constructor(private httpCaller: HttpCaller,
+    private settingsService: SettingsService) { }
 
-  getCategories(isMain:boolean) : Observable<Category[]>{
+  getCategories(isMain: boolean): Observable<Category[]> {
     return isMain == null
-    ? this.http.get<Category[]>(this.path + 'categories')
-    : this.http.get<Category[]>(this.path + 'categories?isMain='+ isMain);
+      ? this.httpCaller
+        .get(
+          this.settingsService.data.categories,
+          null,
+          true,
+          'There was an error while getting your data'
+        )
+        .map((response: Response) => {
+          let result = response as any;
+          return response as any;
+        })
+      : this.httpCaller
+        .get(
+          this.settingsService.data.categories + "?isMain=" + isMain,
+          null,
+          true,
+          'There was an error while getting your data'
+        )
+        .map((response: Response) => {
+          return response as any;
+        });
   }
 }
--- Angular/PurpleShopping/src/app/services/product.service.ts
+++ Angular/PurpleShopping/src/app/services/product.service.ts
@@ -1,20 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { HttpCaller } from '../common/http-caller';
 import { Product } from '../models/Product';
+import { SettingsService } from './settings.service';
+import { Response } from '@angular/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
-  constructor(private http: HttpClient) {}
-  path = 'https://localhost:44389/api/' + 'products/';
+  constructor(private httpCaller: HttpCaller,
+    private settingsService: SettingsService) { }
 
-  getProduct(categoryId): Observable<Product[]> {
-    let newPath = this.path;
+
+  getProduct(categoryId: any): Observable<Product[]> {
+    let newPath = this.settingsService.data.products;
     if (categoryId) {
       newPath += '?categoryId=' + categoryId;
     }
-    return this.http.get<Product[]>(newPath);
+    return this.httpCaller
+      .get(
+        this.settingsService.data.products,
+        null,
+        true,
+        'There was an error while getting your data'
+      )
+      .map((response: Response) => {
+        return response as any;
+      });
   }
 }
